c77a084abdddef602105f17cff6972cf2f28bec6
NITISH-07-04-05 <149141858+NITISH-07-04-05@users.noreply.github.com>
2026-08-18 00:18:10 +0530

    Add: longest-increasing-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [1] * len(nums)
+
+        for i in range(1,len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = dp[i-j]+1
+
+        return max(dp)+1
