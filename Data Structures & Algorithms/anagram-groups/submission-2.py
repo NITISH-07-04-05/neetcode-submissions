7095d6871553f809bee2040d91cafcdd321479f1
NITISH-07-04-05 <149141858+NITISH-07-04-05@users.noreply.github.com>
2026-08-18 20:44:45 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = {}
+
+        for word in strs:
+            Sorted = "".join(sorted(word))
+
+            if Sorted in hashmap:
+                hashmap[Sorted].append(word)
+
+            else:
+                hashmap[Sorted] = [word]
+        return [word for word in hashmap.values()]
