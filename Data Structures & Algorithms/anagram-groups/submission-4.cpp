20ac28cadbfceafc90c47bf0e7e530817d67bb98
NITISH-07-04-05 <149141858+NITISH-07-04-05@users.noreply.github.com>
2026-08-29 12:56:11 +0530

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map <string,vector<string>> hashmap;
+
+        for (string s : strs){
+            string key = s;
+            sort(key.begin(),key.end());
+            hashmap[key].push_back(s);
+        }
+        vector<vector<string>> result;
+        for (auto & pairs : hashmap){
+            result.push_back(pairs.second);
+        }
+        return result;
+    }
+};
